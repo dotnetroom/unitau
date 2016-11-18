f399bd6e54c60cdfcef573b06e7b1f4c8e57e54f
Felipe <felipe.ssilva@unitau.com.br>
2016-11-18 15:23:30 -0200

    Project MvcFundamentals added
    The first commit of the project.

---
+++ MvcFundamentals/src/MvcFundamentals/Models/Context/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MvcFundamentals.Models.Identity;
+
+namespace MvcFundamentals.Models.Context
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityRole<long>, long>
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
